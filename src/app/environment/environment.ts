f91a17b987615dc01a196edeae7149c9c2dd0154
Estokas <denumalegre4@gmail.com>
2026-02-11 14:23:41 +0800

    push

--- src/app/environment/environment.ts
+++ src/app/environment/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   apiBaseUrl: 'http://localhost:8080/api',
-  githubToken: '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'
+  githubToken: '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'
 };
