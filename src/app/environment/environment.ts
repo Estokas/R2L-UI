1461c63b3c1e69545a92dab7b86d5b74908a9ca6
Estokas <denumalegre4@gmail.com>
2026-02-19 21:39:13 +0800

    sample error

--- src/app/environment/environment.ts
+++ src/app/environment/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   apiBaseUrl: 'http://localhost:8080/api',
-  githubToken: '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'
+  githubToken: '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'
 };
